e841bef4c62a3a24d291a5eb79e2604c1180f9eb
Mariusz Strohschein <mariusz@strohschein.pl>
2015-12-08 21:30:49 +0100

    Remove username

--- open_facebook/api.py
+++ open_facebook/api.py
@@ -851,7 +851,7 @@ class OpenFacebook(FacebookConnection):
         '''
         me = getattr(self, '_me', None)
         if me is None:
-            self._me = me = self.get('me?fields=username,email,name,first_name,last_name,birthday,about,gender')#, languages
+            self._me = me = self.get('me?fields=email,name,first_name,last_name,birthday,about,gender')#, languages
 
         print("debug3 !!! ", me)
         return me
